dc3fa0f6c30782808a6947e6f5cae524df739274
Manda Putra <mandaputra8@gmail.com>
2023-07-26 22:42:45 +0700

    fix: saving files

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {
 import indexedDbAdapter from "native-file-system-adapter/src/adapters/indexeddb.js";
 
 const PRESIGNED_URLS =
-  "https://development-nearbysky-user-contents.s3.ap-southeast-2.amazonaws.com/64bf77f4e5811400230607eb/PPGWDU_4k_video.mp4?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEKH%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaDmFwLXNvdXRoZWFzdC0xIkgwRgIhAIn1H2hme5sPvRyU1qDn4ram8FbL5jhigz9ChRoqOKDaAiEA0xqgYXoEnliamxQM8Ju7CX8vmRFJrpnHHh9f1LvrBvEqhQMIOhAEGgw3NjU4OTU2NDU1MDAiDPjJgKVLV08uFhvFzCriAswqxz3jjdwmQm59swDLYwyGFpptsdIJ%2FlygtmnJ7zH0gMmVsQxfu4Yl0DcfqHrjCEao9wtdqTvv0zdpUG4Lg3NcxB%2F%2F0MHXbtJjEl7xNPvCDzIZZDzTOmEjs0ucuyg3%2FqH3MUFFwl8ZEcyVWBE%2FqVeyL2Gg%2BlEzH4%2FtU3NSoVEk4e54x%2BWwPtUpCxs8IwYjg25qP53zl2tBpPgIy4meLhbI6rH57uHkpi%2FUx2%2Fjt1rButeBSG4FlVgMaHBTFoBgMN2%2FEvrFyPinSWdutp3%2FLlJa%2BMEdd7XhVLP67me%2BS7EatP60EG9LAZn%2FD5NYGpQen9BeIJ1nmwxFRHfBegvPpDIyax97GUVYNvS%2BgI9WluVHQZ47GjhAsmOQEhuicCdXAxdBxquP9RZaSSO6%2FViTzlw5w3GDNqml5bw4kYuED7hoBWYCZFkx0ugT9r%2BovRSJfwQXAAWGopG2hFSHGp4EUyzXezDE3oGmBjqyAj02CzkZ0q9wtLGy8DaMJAL69%2F%2F5z1wcAaCVvT6aDYlb50tyCJ%2BnGaWGtjaLnnst7buNAv7BErJkkeGkTMcHipcPgZPHjGpC1BW7F4V77XPxs82mucT%2Fc5CUYSJfADPPei6%2BG79UUnR9sYJW7DDJIoRKRSrfcIHhZTHuevoxHdNz%2BREjCJz9mbKhiQFh0vd34FTFHygqydUFtpuWi%2FLZ6I%2FaRH0m8ZDa9pDSdwTYXIB5OpJuToA9LkJYWvzIsPcYoIumKYdVvCDrKYzlnJeAqjWmhEGsP3NJFeF6Dqtm0S6762YRkcJmYFQ2zqVoMUJDdU13covJjxJ%2Fzl5ZqHvWAEx%2BDHQu8OMoT1vHqu%2BFyZpctTpaO7N6hQ6NEpQ5cR%2Bw2BlvYSZmrpNjoSqS5epjO%2B9Nkw%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20230726T005838Z&X-Amz-SignedHeaders=host&X-Amz-Expires=43200&X-Amz-Credential=ASIA3EUXQDE6KGVO4IL6%2F20230726%2Fap-southeast-2%2Fs3%2Faws4_request&X-Amz-Signature=444b4abf5f2e317df3db429fb7a4b29e8d64350c66dcbcfa1b55359521a594a2";
+  "https://development-nearbysky-user-contents.s3.ap-southeast-2.amazonaws.com/64bf77f4e5811400230607eb/pexels_videos_4728%20%282160p%29.mp4?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEK%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaDmFwLXNvdXRoZWFzdC0xIkcwRQIgIz78FflFtXodGTnVLXevuCjP0ZrCycqaJlF7JNkBhzsCIQC7I%2FnwYQk6kXb%2BHxWYt7pYlAE%2Bwyn6bQ1cYRf7Dz6EEyqFAwhIEAQaDDc2NTg5NTY0NTUwMCIMxvbXRzx105qm%2BN%2BtKuICEyMw%2FQlkDgLXXg9tPNNWfXEFmz2QeKFFppShrzU2PJBH%2F0%2Bs3OgYqxGMW%2F3t1hadIMXhjGhZwhiykd0OFHNk38bxSBtLnYOZIO7MEWBIpaqIFBX3uU76iHzhWAaEoeMEdtyGisUyBcJTBwMP0HM9%2FamevvgCNgLsclOjdgm2VRVZKXxTqfXyC5UimwvNhLF5qKrjS9uwqgDY%2B3RAhyzhJdMjZqCfHhR9OoJR5C4Ot7zdm98zgM5PF%2Bs%2FdHA7mI5y7d%2FySIu6Ky6h9dGz6FnCtk8bLM9jcO5f0YdB4D3CjiuU3%2FMhr3nsopUcBYBdMJA5f6Tdi%2B%2FSj02krXZ4leBE2C0qKvuwrHooU4u9wBOBjOSuoavk5UgdPw22g9qhvRlxfFhJw2avL3N3OFt5fMKQ4iDid0yW0dzTFSxZDg1%2FmowWFo2CKuxvTIt8ZdcMpL13H6XeKB4e%2FcZ9hKRLrAmuo98wMLbqhKYGOrMCxbNm73eOwv94i8wU5BkjoysslvprbXRnVjka%2FECjHBTxUcqTdUrXCALul5uj2gbnJrIaD%2Bo3sWF8nBCBBVmzfrxDWL0rLoHfMmaJy3RpImPF0VrnK872ZGx1Gs%2BSVo5TuZnKwPhuEu02NoRzXydos96zeVuqltWSSZc%2FMjmpQhhNdqab5NGREZ0NmMj8PDuBeSsHoduhsSOac2i1afFuDhRnzPa6bfZiRS3C8wH%2FhvcdHHseDXx6B1jecn2G8uhmdTbM%2B2q7jCyEQNBLgnGdl6W05Y7YeFmrMFJLj7fpe0LQgQsv81zXrActM1dBSKf5WLEv5tRn8jh435V7FogyldN%2Fres73o9NHKaaP5RA%2ByQetINtsV%2BA9LHGtvqN4RlK%2BxNDO%2BUK5Or%2Bc%2FSL4N0dJb2GIw%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20230726T150140Z&X-Amz-SignedHeaders=host&X-Amz-Expires=43200&X-Amz-Credential=ASIA3EUXQDE6OKSAN5UL%2F20230726%2Fap-southeast-2%2Fs3%2Faws4_request&X-Amz-Signature=d24c155a289be377ea75f853c948c9e98c1fd951168c94065a1be8de5e6e908e";
 const FILENAME = "PPGWDU_4k_video.mp4";
 
 const URL_INPUT = document.querySelector("#url-input") as HTMLInputElement;
@@ -92,11 +92,7 @@ async function downloadInChunks(url: string, filename: string) {
     } else {
       if (res.status === 416) {
         // Remove cache on indexed db if the download cannot be resumed
-        // TODO: The function for removing cache is broken, need to fix it on upstream libs
-        // can always delete the whole database with
-        // indexedDB.deleteDatabase("fileSystem")
-        // although if you have multiple download happen in the same indexedDB it would restart from start too
-        // await filehandle.remove()
+        await dirHandle.removeEntry(filename);
         throw Error("Download Error, Removing Cache");
       } else {
         throw Error("Unknown Error!");
@@ -106,13 +102,22 @@ async function downloadInChunks(url: string, filename: string) {
 
   // Saving on the indexeddb store are done
   await writeable.close();
+  // @ts-expect-error no type
+  await downloadFile(filename, dirHandle);
+}
 
-  // Save on the disk, preferably on /Downloads folder
-  const completedFile = filehandle.getFile();
+async function downloadFile(
+  filename: string,
+  dirHandle: FileSystemDirectoryHandle
+) {
+  const completedFileHandle = await dirHandle.getFileHandle(filename, {
+    create: false,
+  });
+  const completedFile = await completedFileHandle.getFile();
+
   const fileSaveHandler = await showSaveFilePicker({
     _preferPolyfill: false,
-    suggestedName: "Untitled.png",
-    _name: filename,
+    suggestedName: filename,
     accepts: [
       { accept: { "image/png": ["png"] } },
       { accept: { "image/jpg": ["jpg"] } },
@@ -122,11 +127,7 @@ async function downloadInChunks(url: string, filename: string) {
     excludeAcceptAllOption: false, // default
   });
 
-  const writeDisk = await fileSaveHandler.createWritable({
-    keepExistingData: false,
-  });
-  await writeDisk.write(completedFile);
-  await writeDisk.close();
+  await completedFile.stream().pipeTo(await fileSaveHandler.createWritable());
 }
 
 // await downloadInChunks(PRESIGNED_URLS, "PPGWDU_4k_video.mp4");
